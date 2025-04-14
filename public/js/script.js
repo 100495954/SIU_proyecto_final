42f5527bef26fbd6cb59a13bad08b13b0a54b25c
100495680 <100495680@alumnos.uc3m.es>
2025-04-14 16:34:18 +0200

    cambios para que funcione el código aquí tmbn

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 import { inicializarMapa } from './modules/mapa.js';
 import { inicializarPermisos } from './modules/permisos.js';
 import { inicializarPersona } from './modules/persona.js';
+import { io } from "socket.io-client";
 
 // Inicializar elementos
 inicializarMapa();
