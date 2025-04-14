62280d922894812e98b2a57ebea48ab795dd67c3
100495680 <119413050+100495680@users.noreply.github.com>
2025-04-14 14:27:59 +0000

    metidos cuidador.html y los cambios de pantalla en las alertas

--- public/js/script.js
+++ public/js/script.js
@@ -6,3 +6,12 @@ import { inicializarPersona } from './modules/persona.js';
 inicializarMapa();
 inicializarPermisos();
 inicializarPersona();
+
+// Meter estas partes en vuestro código lo dejo aquí temporalmente
+const socket = io(); // Conexión a Socket.io
+document.querySelector('#llamar').addEventListener('click', () => {
+  socket.emit('Alerta', 'Llamada'); // Emitir evento de caída
+})
+document.querySelector('#timbre').addEventListener('click', () => {
+  socket.emit('Alerta', 'Timbre'); // Emitir evento de timbre
+})
